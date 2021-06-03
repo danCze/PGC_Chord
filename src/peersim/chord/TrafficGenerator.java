a287ef3ece05f3a70eeba5ff1e8cd4a505c9d85b
Daniel Czeresnia Etinger <danielczeresnia27@gmail.com>
2021-06-03 09:35:29 -0300

    basic convergence achieved (needs plotting and bug fixes)

--- src/peersim/chord/TrafficGenerator.java
+++ src/peersim/chord/TrafficGenerator.java
@@ -8,6 +8,13 @@ import java.security.NoSuchAlgorithmException;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.util.List;
+import java.util.ArrayList;
+import java.util.AbstractMap;
+import java.util.HashMap;
+
 import peersim.core.*;
 import peersim.config.Configuration;
 import peersim.edsim.EDSimulator;
@@ -66,12 +73,67 @@ public class TrafficGenerator implements Control {
 			if(!transferido) {
 				transferido = true;
 				
-				for(int i = 0; i < Network.size(); i++) {
-					System.out.println("node: " + i);
-					for(int j = 0; j < ChordProtocol.path[i].size(); j++) {
-						System.out.print(ChordProtocol.path[i].get(j) + " ");
+				try {
+					int size = ChordProtocol.path.length;
+					
+					//partial output - paths
+					String filename = "output1.txt";
+					FileWriter myWriter = new FileWriter(filename);
+					
+					for(int i = 0; i < size; i++) {
+						myWriter.write("node: " + i + "\n");
+						for(int j = 0; j < ChordProtocol.path[i].size(); j++) {
+							myWriter.write(ChordProtocol.path[i].get(j) + " ");
+						}
+						myWriter.write("\n");
 					}
-					System.out.println();
+					myWriter.close();
+				    System.out.println("Successfully wrote to the file 1.");
+				
+				    //processing and final(?) output - convergence
+				    filename = "output2.txt";
+				    myWriter = new FileWriter(filename);
+				    
+				    int max = 0;
+				    for(int i = 0; i < size; i++) {
+						if(max < ChordProtocol.path[i].size()) {
+							max = ChordProtocol.path[i].size();
+						}
+					}
+				    HashMap<BigInteger, Integer>[] reversePath = new HashMap[max];
+				    for(int i = 0; i < max; i++) {
+						reversePath[i] = new HashMap<BigInteger, Integer>();
+					}
+				    for(int i = 0; i < max; i++) {
+				    	for(int j = 0; j < size; j++) {
+							int currPeerIdx = ChordProtocol.path[j].size() - 1 - i;
+							if(currPeerIdx >= 0) {
+								BigInteger peer = ChordProtocol.path[j].get(currPeerIdx);
+								if(reversePath[i].containsKey(peer)) {
+									int currTimes = reversePath[i].get(peer);
+									reversePath[i].replace(peer, currTimes + 1);
+								}
+								else {
+									reversePath[i].put(peer, 1);
+								}
+							}
+				    	}
+					}
+				    for(int i = 0; i < max; i++) {
+						myWriter.write("peer: " + i + "\n");
+						//for(int j = 0; j < reversePath[i].size(); j++) {
+						//	myWriter.write(reversePath[i].get(j) + " " +  +"\n");
+						//}
+						for (HashMap.Entry<BigInteger, Integer> pair : reversePath[i].entrySet()) {
+							myWriter.write(String.format("%s %s\n", pair.getKey(), pair.getValue()));
+						}
+						myWriter.write("\n");
+					}
+				    System.out.println("Successfully wrote to the file 2.");
+				}
+				catch(IOException e) {
+					System.out.println("An error occurred.");
+				    e.printStackTrace();
 				}
 			}
 			return false;
@@ -106,4 +168,15 @@ public class TrafficGenerator implements Control {
 		//return false;
 	//}
 
+	public <T> List<T> intersection(List<T> list1, List<T> list2) {
+        List<T> list = new ArrayList<T>();
+
+        for (T t : list1) {
+            if(list2.contains(t)) {
+                list.add(t);
+            }
+        }
+
+        return list;
+    }
 }
